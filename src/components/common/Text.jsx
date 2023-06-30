f2cb106c77e4d5b08088755ef0690c351a15b8d9
edidiesky <essienedidiong1000@gmail.com>
2023-06-30 12:59:52 -0700

    new fixes

--- src/components/common/Text.jsx
+++ src/components/common/Text.jsx
@@ -5,18 +5,20 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 export default function TextIndex({ x }) {
   const lineRef = useRef(null);
+  const textRef = useRef(null);
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
     const lineElement = lineRef.current;
+    const textElement = textRef.current;
     const spanElements = lineElement.querySelectorAll(".span");
-    // gsap.set(lineElement, { width: "100%" })
+    // gsap.set(textElement, { width: "100%" });
     spanElements.forEach((spanElement) => {
       gsap.to(spanElement, {
         width: "0%",
         duration: 1.3,
-        delay:.8,
+        delay: 0.8,
         scrollTrigger: {
           trigger: lineElement,
           start: "top center",
@@ -29,7 +31,7 @@ export default function TextIndex({ x }) {
 
   return (
     <TetxIndexContent>
-      <div className="line">
+      <div className="line" ref={textRef}>
         <h2 id="text" ref={lineRef} className="w-100">
           {x}
           <div className="span"></div>
